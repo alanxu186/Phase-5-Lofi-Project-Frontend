8b324c59657d51d49b0245ab84fe4b31cf699a21
Alan Xu <alan.xu1699@gmail.com>
2022-09-01 23:53:47 -0400

    successful test fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PostContainer from './PostContainer'
 import LeftContainer from './LeftContainer'
 import RightContainer from './RightContainer'
 
 const Home = () => {
+  // const [posts, setPosts] = useState([])
 
-  
+  // useEffect(() => {
+
+  // })
 
   return (
     <div className='flex justify-center h-screen'>
--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -7,15 +7,25 @@ const PostContainer = () => {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    const fetchUser = async () => {
-      const res = await axios.get(`users/$`)
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get('http://localhost:4000/api/posts/find/630e5435b1b5c11f3de3d2fc');
+        console.log(res.data)
+        setPosts(res.data)
+      } catch (error) {
+
+      }
     }
-  })
+    fetchPosts();
+  },[])
+
+  // const mappedPosts = posts.map((item) => (item))
 
   return (
     <div className='w-full lg:w-2/3 xl:w-2/5 pt-32 lg:pt-16 px-2 bg-red-200 '>
       <PostTile/>
-
+      {posts.title}
+    {posts.content}
     </div>
   )
 }
