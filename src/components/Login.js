2dca08b6f7dc442ecfb5fccc7b7597c4ae3a6ebb
Alan Xu <alan.xu1699@gmail.com>
2022-09-03 20:10:34 -0400

    setup part of redux

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,52 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import coffeeshop from "../assets/coffeeshop.gif"
+import { loginStart, loginFailure, loginSuccess } from '../redux/userSlice'
 
 const Login = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [form, showForm] = useState(false)
+    const dispatch = useDispatch()
+    // const [form, showForm] = useState(false)
 
-    const handleForm = () => {
-        showForm(!form)
-    }
+    // const handleForm = () => {
+    //     showForm(!form)
+    // }
 
-    const handleSubmit = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault()
+        dispatch(loginStart())
+        try {
+            const res = await axios.post('http://localhost:4000/api/auth/signin', {email,password})
+            dispatch(loginSuccess(res.data))
+        } catch (err) {
+            dispatch(loginFailure())
+        }
     }
 
     return (
         <div className='flex w-full h-screen'>
             {/* <video src={coffeeshop}/> */}
             <div className='w-full flex items-center justify-center '>
-                <div className='bg-[#E2CCC6] px-10 py-20 rounded-3xl border-gray-200'>
+                <form className='bg-[#E2CCC6] px-10 py-20 rounded-3xl border-gray-200'>
                     <h1 className='text-5xl font-semibold'>Welcome Back</h1>
                     <p className='font-medium text-lg text-gray-500 mt-4'>Welcome back! Please enter your details.</p>
                     <div className='mt-8'>
                         <div className='text-lg font-medium'>
                             <label>Email</label>
-                            <input className='w-full border-2 border-gray-500 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your email' onChange={(e) => { setEmail(e.target.value) }} />
+                            <input className='w-full border-2 border-gray-500 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your email' type='email' required onChange={(e) => { setEmail(e.target.value) }} />
                         </div>
                         <div className='text-lg font-medium'>
                             <label>Password</label>
-                            <input className='w-full border-2 border-gray-500 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your password' type='password' onChange={(e) => { setPassword(e.target.value) }} />
+                            <input className='w-full border-2 border-gray-500 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your password' type='password' required onChange={(e) => { setPassword(e.target.value) }} />
                         </div>
                         <div className='mt-8 flex flex-col gap-y-4'>
-                            <button className='active:scale-[0.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-4 rounded-xl bg-[#EF798E] text-white text-lg font-bold'>Login</button>
+                            <button className='active:scale-[0.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-4 rounded-xl bg-[#EF798E] text-white text-lg font-bold' onClick={handleLogin}>Login</button>
                         </div>
                     </div>
-                </div>
+                </form>
 
                 {/* <div className='w-full flex items-center justify-center '>
                     <div className='bg-[#E2CCC6] px-10 py-20 rounded-3xl border-gray-200'>
@@ -57,30 +68,13 @@ const Login = () => {
                                 <input className='w-full border-2 border-gray-500 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your password' type='password' onChange={(e) => { setPassword(e.target.value) }} />
                             </div>
                             <div className='mt-8 flex flex-col gap-y-4'>
-                                <button className='active:scale-[0.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-4 rounded-xl bg-[#EF798E] text-white text-lg font-bold'>Login</button>
+                                <button className='active:scale-[0.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-4 rounded-xl bg-[#EF798E] text-white text-lg font-bold'>Create account</button>
                             </div>
                         </div>
                     </div>
 
                 </div> */}
-
-                {/* <div className='hidden relative lg:flex w-1/2 h-full items-center justify-center bg-gray-200'>
-                <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full'></div>
-                <div className='w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg'></div>
-            </div> */}
-
             </div>
-            {/* // <div>
-        //     <div>
-        //         <h1>Sign In</h1>
-        //         <input placeholder='email' onChange={(e) => setEmail(e.target.value)} />
-        //         <input placeholder='password' onChange={(e) => setPassword(e.target.value)} />
-        //         <button onSubmit={handleSubmit}>Sign In</button>
-
-        //         <h1>Create an account</h1>
-        //         <input placeholder='Name' onChange={(e) => setName(e.target.value)} />
-        //     </div>
-        // </div> */}
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
